e0ffa81d709fc1343b31aa35e2b58c13a8205c8f
slinden2 <39922426+slinden2@users.noreply.github.com>
2024-06-10 22:03:55 +0200

    Commit

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -3,9 +3,22 @@ import prisma from "@/prisma/db";
 import DataTable from "./DataTable";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import Pagination from "@/components/Pagination";
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+interface SearchParams {
+  page: string;
+}
+
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const ticketCount = await prisma.ticket.count();
+
+  const tickets = await prisma.ticket.findMany({
+    take: pageSize,
+    skip: (page - 1) * pageSize,
+  });
+
   return (
     <div>
       <Link
@@ -14,6 +27,11 @@ const Tickets = async () => {
         New Ticket
       </Link>
       <DataTable tickets={tickets} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
